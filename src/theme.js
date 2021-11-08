429b697c2f1dde770b715c09763614e680cbf646
zecazeco <joseluishaas@gmail.com>
2021-11-08 15:11:44 -0300

    primeiro commit

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#000014',
+    text: '#0000b3',
+    success: '#135e01',
+  },
+  fonts: {
+    regular: 'Roboto, sans-serif'
+  },
+};
+
+export default theme;
