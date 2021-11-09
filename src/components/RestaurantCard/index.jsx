f49f92b1d4ed80ae5a8b66637b311b2ff7fc7fd6
zecazeco <joseluishaas@gmail.com>
2021-11-09 20:40:15 -0300

    ajustes estilo dos detalhes do restaurante

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import ReactStars from "react-rating-stars-component";
-import { Restaurant, RestaurantInfo, Content, Text, RestaurantPhoto } from './styles';
+import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles';
 
 
 const ResturantCard = ({restaurant}) => {
   return (
     <Restaurant>
       <RestaurantInfo>
-        <Text>{restaurant.name ? restaurant.name : 'nome'}</Text>
+        <Title>{restaurant.name ? restaurant.name : 'nome'}</Title>
         <ReactStars count={5} value={restaurant.rating} edit={false} isHalf activeColor="#e7711c" />
-        <Content>{restaurant.formatted_address || restaurant.vicinity}</Content>
+        <Address>{restaurant.formatted_address || restaurant.vicinity}</Address>
       </RestaurantInfo>
       <RestaurantPhoto
         src={restaurant.photos ? restaurant.photos[0].getUrl() : restaurant.icon}
--- src/components/RestaurantCard/styles.js
+++ src/components/RestaurantCard/styles.js
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 //import Text from '../Text';
 
-export const Text = styled.h1`
-  line-height: 19px;
-  font-size: 16px;
-  font-weight: medium;
-  letter-spacing: 0.15px;
-`;
-
 export const Restaurant = styled.div`
   display: flex;
   justify-content: space-between;
@@ -27,13 +20,27 @@ export const RestaurantInfo = styled.div`
   flex-direction: column;
 `;
 
+export const Title = styled.span`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin-bottom: 10px;
+`;
+
+export const Address = styled.span`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  line-height: 19px;
+  font-size: 16px;
+  letter-spacing: 0.15px;
+  margin: 10px 0px;
+`;
+
 export const RestaurantPhoto = styled.img`
   border-radius: 6px;
   width: 100px;
   height: 100px;
   object-fit: cover;
-`;
-
-export const Content = styled(Text)`
-  margin-top: 5px;
 `;
